f273a53beb3619a5f4d4c1bb1a6134632159cb43
Jingbei Li <i@jingbei.li>
2021-08-18 17:28:54 +0800

    updated models

--- model/neufa.py
+++ model/neufa.py
@@ -2,7 +2,9 @@ import torch
 from torch.autograd import Variable
 from torch import nn
 from torch.nn.utils.rnn import pad_sequence
-from .modules import Encoder, ReferenceEncoder, Decoder
+from .modules import Tacotron2Encoder, ReferenceEncoder, ContentEncoder, Decoder, GumbelSoftmax
+from .test import Encoder as TestEncoder
+from .test import Decoder as TestDecoder
 from .attention import BidirectionalAttention, BidirectionalAdditiveAttention
 from .position import PositionalEncoding
 
@@ -10,34 +12,29 @@ class BidirectionalAttention(BidirectionalAttention):
 
     def __init__(self, input1_dim, input2_dim, attention_dim):
         super().__init__(input1_dim, input2_dim, input1_dim, input2_dim, attention_dim)
-
-    def forward(self, input1, input2):
-        return super().forward(input1, input2, input1, input2)
+        #self.softmax2 = GumbelSoftmax(dim=1)
 
 class BidirectionalAdditiveAttention(BidirectionalAdditiveAttention):
 
     def __init__(self, input1_dim, input2_dim, attention_dim):
         super().__init__(input1_dim, input2_dim, input1_dim, input2_dim, attention_dim)
 
-    def forward(self, input1, input2):
-        return super().forward(input1, input2, input1, input2)
-
 class NeuFA_base(nn.Module):
 
     def __init__(self, hparams):
         super().__init__()
 
+        self.scale_factor = 1 / hparams.scale_factor
         self.layer_normalization = nn.LayerNorm(hparams.input.mfcc_dim)
 
-        self.text_encoder = Encoder(hparams.text_encoder)
-        self.speech_encoder = ReferenceEncoder(hparams.speech_encoder)
+        self.text_encoder = Tacotron2Encoder(hparams.text_encoder)
+        self.speech_encoder = ContentEncoder(hparams.speech_encoder)
         #self.attention = BidirectionalAdditiveAttention(hparams.text_encoder.output_dim, hparams.speech_encoder.output_dim, hparams.attention.dim)
         self.attention = BidirectionalAttention(hparams.text_encoder.output_dim, hparams.speech_encoder.output_dim, hparams.attention.dim)
         self.text_decoder = Decoder(hparams.text_decoder)
         self.speech_decoder = Decoder(hparams.speech_decoder)
         self.positional_encoding_text = PositionalEncoding(hparams.text_encoder.output_dim)
         self.positional_encoding_speech = PositionalEncoding(hparams.speech_encoder.output_dim)
-        self.softmax = nn.Softmax(-1)
 
         self.mse = nn.MSELoss()
         self.cross_entrophy = torch.nn.CrossEntropyLoss()
@@ -46,39 +43,59 @@ class NeuFA_base(nn.Module):
         text_lengths = [i.shape[0] for i in texts]
         texts = pad_sequence(texts, batch_first=True)
         texts = self.text_encoder(texts, text_lengths)
+        texts = torch.cat([texts, torch.zeros((texts.shape[0], 1, texts.shape[2])).to(texts.device)], axis=-2)
 
         mfcc_lengths = [i.shape[0] for i in mfccs]
         mfccs = pad_sequence(mfccs, batch_first=True)
+
+        if self.scale_factor < 1:
+            mfccs = nn.functional.interpolate(mfccs.transpose(1, 2), scale_factor=self.scale_factor).transpose(1, 2)
+            mfcc_lengths = [int(i * self.scale_factor) for i in mfcc_lengths]
+
         normalized_mfccs = self.layer_normalization(mfccs)
-        mfccs = self.speech_encoder(normalized_mfccs)
+        mfccs = self.speech_encoder(normalized_mfccs, mfcc_lengths)
+        #mfccs = self.speech_encoder(mfccs, mfcc_lengths)
+        mfccs = torch.cat([mfccs, torch.zeros((mfccs.shape[0], 1, mfccs.shape[2])).to(mfccs.device)], axis=-2)
 
-        texts = self.positional_encoding_text(texts)
-        mfccs = self.positional_encoding_speech(mfccs)
+        normalized_mfccs = [mfcc[:mfcc_lengths[i]] for i, mfcc in enumerate(normalized_mfccs)]
 
-        texts_at_frame, mfccs_at_text, w1, w2 = self.attention(texts, mfccs)
+        texts_pe = self.positional_encoding_text(texts)
+        mfccs_pe = self.positional_encoding_speech(mfccs)
+
+        texts_at_frame, mfccs_at_text, w1, w2 = self.attention(texts_pe, mfccs_pe, texts, mfccs, text_lengths, mfcc_lengths)
 
         texts = self.text_decoder(mfccs_at_text, text_lengths)
         mfccs = self.speech_decoder(texts_at_frame, mfcc_lengths)
 
-        texts = self.softmax(texts)
+        return texts, mfccs, w1, w2, normalized_mfccs
 
-        return texts, mfccs, w1[0][:mfcc_lengths[0], :text_lengths[0]], w2[0][:text_lengths[0], :mfcc_lengths[0]], normalized_mfccs
-
-    def loss(self, p_texts, p_mfccs, texts, mfccs):
+    def text_loss(self, p_texts, texts):
         text_lengths = [i.shape[0] for i in texts]
-        mfcc_lengths = [i.shape[0] for i in mfccs]
-
         p_texts = [p_text[:text_lengths[i]] for i, p_text in enumerate(p_texts)]
-        p_mfccs = [p_mfcc[:mfcc_lengths[i]] for i, p_mfcc in enumerate(p_mfccs)]
-        mfccs = [p_mfcc[:mfcc_lengths[i]] for i, p_mfcc in enumerate(mfccs)]
-
         p_texts = torch.cat(p_texts)
-        p_mfccs = torch.cat(p_mfccs)
-
         texts = torch.cat(texts)
-        mfccs = torch.cat(mfccs)
+        return self.cross_entrophy(p_texts, texts)
 
-        return self.mse(p_mfccs, mfccs) + self.cross_entrophy(p_texts, texts)
+    def mfcc_loss(self, p_mfccs, mfccs):
+        mfcc_lengths = [i.shape[0] for i in mfccs]
+        p_mfccs = [p_mfcc[:mfcc_lengths[i]] for i, p_mfcc in enumerate(p_mfccs)]
+        p_mfccs = torch.cat(p_mfccs)
+        mfccs = torch.cat(mfccs)
+        return self.mse(p_mfccs, mfccs)
+
+    def attention_loss(self, w1, w2, alpha=1):
+        loss = []
+        for _w1, _w2 in zip(w1, w2):
+            w = torch.maximum(_w1.T, _w2)
+            a = torch.linspace(1e-6, 1, w.shape[0]).to(w.device).repeat(w.shape[1], 1).T
+            b = torch.linspace(1e-6, 1, w.shape[1]).to(w.device).repeat(w.shape[0], 1)
+            r1 = torch.maximum((a / b), (b / a))
+            r2 = torch.maximum(a.flip(1) / b.flip(0), b.flip(0)/ a.flip(1))
+            r = torch.maximum(r1, r2) - 1
+            r = torch.tanh(alpha * r)
+            loss.append(torch.mean(w * r.detach()))
+        loss = torch.stack(loss)
+        return torch.mean(loss)
 
 if __name__ == '__main__':
     import os
@@ -86,17 +103,18 @@ if __name__ == '__main__':
     from hdfs import HDFSLoader, HDFSCollate
     from hparams import base
 
-    device = 'cuda:6'
-    batch_size = 2
+    device = 'cuda:1'
+    batch_size = 4
 
-    dataset = LibriSpeech(os.path.expanduser('~/datasets/LibriSpeech/packed/LibriSpeech'))
+    dataset = LibriSpeech('hdfs://haruna/home/byte_speech_sv/user/lijingbei/LibriSpeech/packed/LibriSpeech')
     data_loader = HDFSLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=HDFSCollate(device), drop_last=True, num_readers=batch_size)
 
     for batch in data_loader:
         model = NeuFA_base(base)
         model.to(device)
         output = model(*batch)
-        print([i.shape for i in output])
-        loss = model.loss(*output[:2], batch[0], output[-1])
-        print(loss)
+        #print([i.shape for i in output])
+        print(model.text_loss(output[0], batch[0]))
+        print(model.mfcc_loss(output[1], output[-1]))
+        print(model.attention_loss(output[2], output[3]))
         break
--- model/test.py
+++ model/test.py
@@ -2,7 +2,7 @@ import torch
 from torch.autograd import Variable
 from torch import nn
 from torch.nn.utils.rnn import pad_sequence
-from .modules import TacotronEncoder, Tacotron2Encoder, ReferenceEncoder, Decoder
+from .modules import TacotronEncoder, Tacotron2Encoder, ReferenceEncoder, Decoder, GumbelSoftmax, MixedGumbelSoftmax
 from .attention import BidirectionalAttention, BidirectionalAdditiveAttention
 from .position import PositionalEncoding
 
@@ -10,17 +10,31 @@ class BidirectionalAttention(BidirectionalAttention):
 
     def __init__(self, input1_dim, input2_dim, attention_dim):
         super().__init__(input1_dim, input2_dim, input1_dim, input2_dim, attention_dim)
-
-    def forward(self, input1, input2):
-        return super().forward(input1, input2, input1, input2)
+        self.softmax1 = GumbelSoftmax(dim=1)
+        self.softmax2 = GumbelSoftmax(dim=1)
 
 class BidirectionalAdditiveAttention(BidirectionalAdditiveAttention):
 
     def __init__(self, input1_dim, input2_dim, attention_dim):
         super().__init__(input1_dim, input2_dim, input1_dim, input2_dim, attention_dim)
 
-    def forward(self, input1, input2):
-        return super().forward(input1, input2, input1, input2)
+class Encoder(torch.nn.Module):
+
+    def __init__(self, hparams):
+        super().__init__()
+        self.embedding = torch.nn.Embedding(hparams.num_symbols, hparams.embedding_dim)
+
+    def forward(self, inputs, input_lengths=None):
+        return self.embedding(inputs)
+
+class Decoder(torch.nn.Module):
+
+    def __init__(self, hparams):
+        super().__init__()
+        self.linear = torch.nn.Linear(hparams.input_dim, hparams.output_dim)
+
+    def forward(self, inputs, input_lengths):
+        return self.linear(inputs)
 
 class BidirectionalAttentionTest(nn.Module):
 
@@ -32,7 +46,6 @@ class BidirectionalAttentionTest(nn.Module):
         self.attention = BidirectionalAdditiveAttention(hparams.text_encoder.output_dim, hparams.text_encoder.output_dim, hparams.attention.dim)
         self.decoder1 = Decoder(hparams.text_decoder)
         self.decoder2 = Decoder(hparams.text_decoder)
-        self.softmax = nn.Softmax(-1)
         self.positional_encoding = PositionalEncoding(hparams.text_encoder.output_dim)
 
         self.cross_entrophy = torch.nn.CrossEntropyLoss()
@@ -41,19 +54,27 @@ class BidirectionalAttentionTest(nn.Module):
         text_lengths = [i.shape[0] for i in texts]
         texts = pad_sequence(texts, batch_first=True)
         texts1 = self.encoder1(texts, text_lengths)
-        texts1 = self.positional_encoding(texts1)
+        texts1_pe = self.positional_encoding(texts1)
         texts2 = self.encoder2(texts, text_lengths)
-        texts2 = self.positional_encoding(texts2)
+        texts2_pe = self.positional_encoding(texts2)
 
-        text1, texts2, w1, w2 = self.attention(texts1, texts2)
+        text1, texts2, w1, w2 = self.attention(texts1_pe, texts2_pe, texts1, texts2, text_lengths, text_lengths)
 
         texts1 = self.decoder1(texts1, text_lengths)
         texts2 = self.decoder2(texts2, text_lengths)
 
-        texts1 = self.softmax(texts1)
-        texts2 = self.softmax(texts2)
+        #return texts1, texts2, w1[0][:text_lengths[0], :text_lengths[0]], w2[0][:text_lengths[0], :text_lengths[0]]
+        return texts1, texts2, w1, w2
 
-        return texts1, texts2, w1[0][:text_lengths[0], :text_lengths[0]], w2[0][:text_lengths[0], :text_lengths[0]]
+    def diagonal_attention_loss(self, w1, w2, k1_lengths, k2_lengths):
+        mask = torch.zeros(attention.shape, dtype=torch.int).detach().to(score.device)
+
+        for i, l1, l2 in enumerate(zip(k1_lengths, k2_lengths)):
+
+            mask[i,l:,:] += 1
+        for i, l in enumerate(k2_lengths):
+            mask[i,:,l:] += 1
+
 
     def loss(self, p_texts1, p_texts2, texts):
         text_lengths = [i.shape[0] for i in texts]
@@ -68,6 +89,20 @@ class BidirectionalAttentionTest(nn.Module):
 
         return self.cross_entrophy(p_texts1, texts) + self.cross_entrophy(p_texts2, texts)
 
+    def attention_loss(self, w1, w2, alpha=1):
+        loss = []
+        for _w1, _w2 in zip(w1, w2):
+            w = torch.maximum(_w1.T, _w2)
+            a = torch.linspace(1e-6, 1, w.shape[0]).to(w.device).repeat(w.shape[1], 1).T
+            b = torch.linspace(1e-6, 1, w.shape[1]).to(w.device).repeat(w.shape[0], 1)
+            r1 = torch.maximum((a / b), (b / a))
+            r2 = torch.maximum(a.flip(1) / b.flip(0), b.flip(0)/ a.flip(1))
+            r = torch.maximum(r1, r2) - 1
+            r = torch.tanh(alpha * r)
+            loss.append(torch.mean(w * r.detach()))
+        loss = torch.stack(loss)
+        return torch.mean(loss)
+
 class BidirectionalAttentionTest2(BidirectionalAttentionTest):
 
     def __init__(self, hparams):
@@ -76,21 +111,21 @@ class BidirectionalAttentionTest2(BidirectionalAttentionTest):
 
 if __name__ == '__main__':
     import os
-    from dataset import LibriSpeech
+    from dataset import LibriSpeechText
     from hdfs import HDFSLoader, HDFSCollate
     from hparams import test
 
-    device = 'cuda:6'
+    device = 'cuda:1'
     batch_size = 2
 
-    dataset = LibriSpeech(os.path.expanduser('~/datasets/LibriSpeech/packed/LibriSpeech'))
+    dataset = LibriSpeechText('hdfs://haruna/home/byte_speech_sv/user/lijingbei/LibriSpeech/packed/LibriSpeech')
     data_loader = HDFSLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=HDFSCollate(device), drop_last=True, num_readers=batch_size)
 
     for batch in data_loader:
         model = BidirectionalAttentionTest2(test)
         model.to(device)
         output = model(batch[0])
-        print([i.shape for i in output])
-        loss = model.loss(*output[:2], batch[0])
-        print(loss)
+        #print([i.shape for i in output])
+        print(model.loss(*output[:2], batch[0]))
+        print(model.attention_loss(*output[2:4]))
         break
